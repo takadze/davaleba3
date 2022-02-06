c8303c7b721764cea8e3616bd28c2bc4f52574c3
takadze <63195640+takadze@users.noreply.github.com>
2022-02-06 10:50:51 -0800

    https://github.com/takadze/davaleba3.git

---
+++ my.js
@@ -0,0 +1,8 @@
+
+var togle = document.getElementById('jaba')
+var hidenav = document.getElementById('hidenav')
+
+togle.addEventListener('click',function(){
+    hidenav.classList.add('hide-show');
+});
+
